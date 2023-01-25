5c8e3c7f5bc82bce5c77319c7a4481f80961e3e7
AmanJain18 <89217032+AmanJain18@users.noreply.github.com>
2023-01-25 06:04:14 +0530

    mod 1 rest-apis

---
+++ validators/productValidator.js
@@ -0,0 +1,10 @@
+import Joi from "joi";
+
+const productSchema = Joi.object({
+  name: Joi.string().required(),
+  category: Joi.string().required(),
+  price: Joi.number().required(),
+  image: Joi.string(),
+});
+
+export default productSchema;
